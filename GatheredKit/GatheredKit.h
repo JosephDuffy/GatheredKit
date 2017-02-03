7c22b91a2aa4966bc73f7b2b44db9b5e40a7221c
Joseph Duffy <josephduffy@me.com>
2017-02-03 18:23:35 +0000

    Initial commit

---
+++ GatheredKit/GatheredKit.h
@@ -0,0 +1,15 @@
+//
+//  GatheredKit.h
+//  GatheredKit
+//
+//  Created by Joseph Duffy on 03/02/2017.
+//  Copyright © 2017 Joseph Duffy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GatheredKit.
+FOUNDATION_EXPORT double GatheredKitVersionNumber;
+
+//! Project version string for GatheredKit.
+FOUNDATION_EXPORT const unsigned char GatheredKitVersionString[];
