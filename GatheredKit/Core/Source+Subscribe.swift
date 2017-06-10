c9a6ac48d899875018cbdd1203f704ff7e434d6b
Joseph Duffy <josephduffy@me.com>
2017-06-10 16:51:35 +0100

    Added `updateInterval` parameter to `subscribeToChanges`

--- GatheredKit/Core/Source+Subscribe.swift
+++ GatheredKit/Core/Source+Subscribe.swift
@@ -19,12 +19,13 @@ public extension Source {
      When the source updates its properties the `listener` closure will be called with the first parameter as the source and the second
      parameter a closure that can be called to unsubscribe
 
-     - parameter queue: The operation queue to which listener closure should be added. Default to main. If you pass nil, the closure is run synchronously on the posting thread.
+     - parameter queue: The operation queue to which listener closure should be added. Default to main. If you pass nil, the closure is run synchronously on the posting thread
+     - parameter updateInterval: If non-nil will start updating source automatically. For `CustomisableUpdateIntervalSource` instances `updateInterval` is used. For `AutomaticallyUpdatingSource` this is ignored
      - parameter listener: The closure to be called when the source's properties are updated
      - returns: A closure that when called will unsubscribe the listener from changes
      */
     @discardableResult
-    public func subscribeToChanges(queue: OperationQueue? = .main, listener: @escaping (Self, Unsubscriber) -> Void) -> Unsubscriber {
+    public func subscribeToChanges(queue: OperationQueue? = .main, updateInterval: Int? = nil, listener: @escaping (Self, Unsubscriber) -> Void) -> Unsubscriber {
         var observer = NotificationCenter.default.addObserver(forName: .sourcePropertiesUpdated, object: self, queue: queue) { [weak self] notification in
             guard let `self` = self else {
                 unsubscribe()
@@ -36,6 +37,14 @@ public extension Source {
 
         func unsubscribe() {
             NotificationCenter.default.removeObserver(observer)
+        }
+
+        if let updateInterval = updateInterval {
+            if let automaticallyUpdatingSource = self as? AutomaticallyUpdatingSource {
+                automaticallyUpdatingSource.startMonitoring()
+            } else if let customisableUpdateIntervalSource = self as? CustomisableUpdateIntervalSource {
+                customisableUpdateIntervalSource.startUpdating(every: updateInterval)
+            }
         }
 
         return unsubscribe
