21136bff1615be68911734542d30f2502e421036
Joseph Duffy <josephduffy@me.com>
2018-06-05 22:23:57 +0100

    Updates `SourcePropertyUnit`'s `formattedString(for:)` to be generic
    Updates `SourceProperty` to have an associated type `UnitType` that conforms to `SourcePropertyUnit`

--- Source/Units/Pixel.swift
+++ Source/Units/Pixel.swift
@@ -5,7 +5,7 @@
 public struct Pixel: NumberBasedSourcePropertyUnit {
 
     /// The value for `maximumFractionDigits` that will be used with the empty initialiser
-    public static let defaultMaximumFractionDigits: Int = 0
+    public let maximumFractionDigits: Int = 0
 
     /// The string that will be appended to the end of the string when
     /// the value does equal 1
@@ -14,16 +14,5 @@ public struct Pixel: NumberBasedSourcePropertyUnit {
     /// The string that will be appended to the end of the string when
     /// the value does not equal 1
     public let pluralValueSuffix = " Pixels"
-
-    /// The maximum number of digits to show after the decimal place
-    public let maximumFractionDigits: Int
-
-    /**
-     Create a new instance of the unit
-     - parameter maximumFractionDigits: The maximum number of digits to show after the decimal place
-     */
-    public init(maximumFractionDigits: Int) {
-        self.maximumFractionDigits = maximumFractionDigits
-    }
 
 }
