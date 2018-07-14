782f418edbcc1ebb2b5125ac5418b70594a1ea86
Joseph Duffy <josephduffy@me.com>
2018-07-14 15:31:44 +0100

    Updates documentation around dispatch queues

--- Source/Sources/ControllableSource.swift
+++ Source/Sources/ControllableSource.swift
@@ -28,12 +28,29 @@ public extension ControllableSource {
      The returned object must be retained; the lifecycle of the listener is tied to the object. If
      the object is deallocated the listener will be destroyed.
 
+     - parameter queue: The dispatch queue the listener should be called from
      - parameter updateListener: The closure to call with updated values
-     - parameter queue: The queue to send updates on
      - returns: An opaque object. The lifecycle of the listener is tied to the object
      */
-    func startUpdating(sendingUpdatesTo updateListener: @escaping UpdateListener, on queue: DispatchQueue) -> AnyObject {
+    func startUpdating(sendingUpdatesOn queue: DispatchQueue, to updateListener: @escaping UpdateListener) -> AnyObject {
         let listenerToken = addUpdateListener(updateListener, queue: queue)
+        startUpdating()
+        return listenerToken
+    }
+
+    /**
+     Starts automatic updates and adds a closure to the array of closures that will be called when
+     any of the source's values are updated. The closure will be called on the main dispatch queue with
+     all values, but not all the values will neccessary be new.
+
+     The returned object must be retained; the lifecycle of the listener is tied to the object. If
+     the object is deallocated the listener will be destroyed.
+
+     - parameter updateListener: The closure to call with updated values
+     - returns: An opaque object. The lifecycle of the listener is tied to the object
+     */
+    func startUpdating(sendingUpdatesTo updateListener: @escaping UpdateListener) -> AnyObject {
+        let listenerToken = addUpdateListener(updateListener, queue: .main)
         startUpdating()
         return listenerToken
     }
