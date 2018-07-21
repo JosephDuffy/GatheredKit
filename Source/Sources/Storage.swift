2b63dad8b38804033a965eed813c514523fc9be7
Joseph Duffy <josephduffy@me.com>
2018-07-21 11:34:35 +0100

    Adds Storage source

---
+++ Source/Sources/Storage.swift
@@ -0,0 +1,59 @@
+import Foundation
+
+public final class Storage: BasePollingSource, Source, ManuallyUpdatableValuesProvider {
+
+    public static let availability: SourceAvailability = .available
+
+    public let displayName = "Storage"
+
+    public private(set) var capacity: GenericValue<Int64?, Byte>
+
+    public private(set) var available: GenericValue<Int64?, Byte>
+
+    public private(set) var free: GenericValue<Int64?, Byte>
+
+    public var allValues: [AnyValue] {
+        return [
+            capacity.asAny(),
+            available.asAny(),
+            free.asAny(),
+        ]
+    }
+
+    public override init() {
+        capacity = GenericValue(displayName: "Capacity", unit: Byte(countStyle: .file))
+        available = GenericValue(displayName: "Available", unit: Byte(countStyle: .file))
+        free = GenericValue(displayName: "Free", unit: Byte(countStyle: .file))
+    }
+
+    public func updateValues() -> [AnyValue] {
+        let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+
+        guard let path = paths.first else { return allValues }
+
+        do {
+            let dictionary = try FileManager.default.attributesOfFileSystem(forPath: path)
+            let capacity = dictionary[FileAttributeKey.systemSize] as? NSNumber
+            let free = dictionary[FileAttributeKey.systemFreeSize] as? NSNumber
+
+            if capacity != nil {
+                self.capacity.update(backingValue: capacity?.int64Value)
+            }
+
+            if free != nil {
+                self.free.update(backingValue: free?.int64Value)
+            }
+
+            if let capacity = capacity, let free = free {
+                let available = capacity.int64Value - free.int64Value
+                self.available.update(backingValue: available)
+            }
+
+        } catch {
+            print("Error getting Storage data", error)
+        }
+
+        return allValues
+    }
+
+}
