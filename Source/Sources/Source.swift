124026333814f417a30e1cb260f3294887543ed1
Joseph Duffy <josephduffy@me.com>
2018-07-28 15:57:15 +0100

    Moves `isUpdating` property from `Source` to `Controllable`

--- Source/Sources/Source.swift
+++ Source/Sources/Source.swift
@@ -8,9 +8,6 @@ public protocol Source: class, ValuesProvider {
     /// The availability of the source
     static var availability: SourceAvailability { get }
 
-    /// A boolean indicating if the source is currently performing automatic updates
-    var isUpdating: Bool { get }
-
     /// A user-friendly name that represents the source, e.g. "Location", "Device Attitude"
     var displayName: String { get }
 
