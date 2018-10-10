57c0f778e19f902534cb3251c764387b30d3950c
Joseph Duffy <josephduffy@me.com>
2018-10-10 01:11:53 +0100

    Fix Dangerfile to only use first range in each match
    Improve output of warnings

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -4,6 +4,15 @@ import Danger
 func checkSwiftVersions() {
     var swiftVersions: [String] = []
 
+    func check(version: String, source: String) {
+        if let first = swiftVersions.first, !swiftVersions.contains(version) {
+            warn("Found mismatched version. Expected " + first + ", found " + version + " in " + source)
+            swiftVersions.append(version)
+        } else if swiftVersions.isEmpty {
+            swiftVersions.append(version)
+        }
+    }
+
     let projectPath = "./GatheredKit.xcodeproj/project.pbxproj"
     if let data = FileManager.default.contents(atPath: projectPath) {
         guard let content = String(data: data, encoding: .utf8) else {
@@ -14,13 +23,13 @@ func checkSwiftVersions() {
         do {
             let regex = try NSRegularExpression(pattern: "SWIFT_VERSION = (.*);", options: [])
             let matches = regex.matches(in: content, options: [], range: NSRange(location: 0, length: content.count))
-            swiftVersions += matches.flatMap { match in
-                return (0..<match.numberOfRanges).map { rangeIndex in
-                    let range = match.range(at: rangeIndex)
-                    let startIndex = content.index(content.startIndex, offsetBy: range.location)
-                    let indexPastEnd = content.index(content.startIndex, offsetBy: range.location + range.length)
-                    return String(content[startIndex..<indexPastEnd])
-                }
+            matches.forEach { match in
+                guard match.numberOfRanges > 0 else { return }
+                let range = match.range(at: 0)
+                let startIndex = content.index(content.startIndex, offsetBy: range.location)
+                let indexPastEnd = content.index(content.startIndex, offsetBy: range.location + range.length)
+                let version = String(content[startIndex..<indexPastEnd])
+                check(version: version, source: projectPath)
             }
         } catch {
             warn("Error creating swift version check regex: \(error.localizedDescription)")
@@ -35,7 +44,7 @@ func checkSwiftVersions() {
             warn("Could not decode data at " + swiftVersionPath + " as UTF8")
             return
         }
-        swiftVersions.append(content)
+        check(version: content, source: swiftVersionPath)
     } else {
         warn("Unable to find swift version file at " + swiftVersionPath)
     }
