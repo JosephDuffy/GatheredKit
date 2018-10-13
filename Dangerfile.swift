f7138c2ffcb1da5c3dfdb96214523cf2c2ca59db
Joseph Duffy <josephduffy@me.com>
2018-10-13 14:21:12 +0100

    Update Dangerfile to check Podspec
    Refactor Dangerfile to allow for arbitrary version checks

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,62 +1,94 @@
 import Foundation
 import Danger
 
-func checkSwiftVersions() {
-    var swiftVersions: [String] = []
-
-    func check(version: String, source: String) {
-        if let first = swiftVersions.first, !swiftVersions.contains(version) {
-            warn("Found mismatched version. Expected " + first + ", found " + version + " in " + source)
-            swiftVersions.append(version)
-        } else if swiftVersions.isEmpty {
-            swiftVersions.append(version)
-        }
+struct VersionFile {
+    enum Interpreter {
+        case regex(String)
+        case closure((String) -> String?)
     }
 
-    let projectPath = "./GatheredKit.xcodeproj/project.pbxproj"
-    if let data = FileManager.default.contents(atPath: projectPath) {
+    let path: String
+    let interpreter: Interpreter
+
+    func getVersions() -> [String] {
+        guard let data = FileManager.default.contents(atPath: path) else {
+            warn("Unable to find file at " + path)
+            return []
+        }
         guard let content = String(data: data, encoding: .utf8) else {
-            warn("Could not decode data at " + projectPath + " as UTF8")
-            return
+            warn("Could not decode data at " + path + " as UTF8")
+            return []
         }
 
-        do {
-            let regex = try NSRegularExpression(pattern: "SWIFT_VERSION = (.*);", options: [])
-            let matches = regex.matches(in: content, options: [], range: NSRange(location: 0, length: content.count))
-            matches.forEach { match in
-                guard match.numberOfRanges > 1 else { return }
-                let range = match.range(at: 1)
-                let startIndex = content.index(content.startIndex, offsetBy: range.location)
-                let indexPastEnd = content.index(content.startIndex, offsetBy: range.location + range.length)
-                let version = String(content[startIndex..<indexPastEnd])
-                check(version: version, source: projectPath)
+        switch interpreter {
+        case .regex(let regex):
+            do {
+                let regex = try NSRegularExpression(pattern: regex, options: [])
+                let matches = regex.matches(in: content, options: [], range: NSRange(location: 0, length: content.count))
+                return matches.compactMap { match in
+                    guard match.numberOfRanges > 1 else {
+                        warn("Failed to find capture group for match \(match)")
+                        return nil
+                    }
+                    let range = match.range(at: 1)
+                    let startIndex = content.index(content.startIndex, offsetBy: range.location)
+                    let indexPastEnd = content.index(content.startIndex, offsetBy: range.location + range.length)
+                    return String(content[startIndex..<indexPastEnd])
+                }
+            } catch {
+                warn("Error creating version check regex from \(regex): \(error.localizedDescription)")
+                return []
             }
-        } catch {
-            warn("Error creating swift version check regex: \(error.localizedDescription)")
+        case .closure(let closure):
+            guard let version = closure(content) else { return [] }
+            return [version]
         }
-    } else {
-        warn("Unable to find project file at " + projectPath)
     }
+}
 
-    let swiftVersionPath = ".swift-version"
-    if let data = FileManager.default.contents(atPath: swiftVersionPath) {
-        guard let content = String(data: data, encoding: .utf8)?.trimmingCharacters(in: .whitespacesAndNewlines) else {
-            warn("Could not decode data at " + swiftVersionPath + " as UTF8")
-            return
+let projectPath = "./GatheredKit.xcodeproj/project.pbxproj"
+let swiftVersionPath = ".swift-version"
+
+func checkSwiftVersions() {
+    check(versionFiles: [
+        VersionFile(path: "./GatheredKit.xcodeproj/project.pbxproj", interpreter: .regex("SWIFT_VERSION = (.*);")),
+        VersionFile(path: ".swift-version", interpreter: .closure({ $0.trimmingCharacters(in: .whitespacesAndNewlines) })),
+    ], versionKind: "Swift")
+}
+
+func checkPodspec() {
+    check(versionFiles: [
+        VersionFile(path: "./Source/Info.plist", interpreter: .regex("<key>CFBundleShortVersionString</key>\\s*<string>(.*)</string>")),
+        VersionFile(path: "./GatheredKit.podspec", interpreter: .regex("version\\s*= \"(.*)\"")),
+    ], versionKind: "framework")
+}
+
+func check(versionFiles: [VersionFile], versionKind: String) {
+    let versions = versionFiles.reduce(Set<String>(), { versions, versionFile in
+        var versions = versions
+
+        let fileVersions = versionFile.getVersions()
+
+        for fileVersion in fileVersions {
+            if let first = versions.first, !versions.contains(fileVersion) {
+                warn("Found mismatched version. Expected " + first + ", found " + fileVersion + " in " + versionFile.path)
+                versions.insert(fileVersion)
+            } else if versions.isEmpty {
+                versions.insert(fileVersion)
+            }
         }
-        check(version: content, source: swiftVersionPath)
-    } else {
-        warn("Unable to find swift version file at " + swiftVersionPath)
-    }
 
-    let uniqueVersions = Set(swiftVersions)
-    if uniqueVersions.isEmpty {
-        warn("Found no swift versions")
-    } else if uniqueVersions.count > 1 {
-        fail("Found mismatch swift versions: \(uniqueVersions)")
+        return versions
+    })
+
+    let paths = versionFiles.map { $0.path }
+
+    if versions.isEmpty {
+        warn("Found no \(versionKind) versions in files: \(paths)")
     } else {
-        message("All Swift versions are in-sync")
+        message("All \(versionKind) versions are in-sync: \(paths)")
     }
 }
 
 checkSwiftVersions()
+checkPodspec()
