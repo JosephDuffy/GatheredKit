25d4cda1194caf55a47ef4c97514e197c310a1dc
Joseph Duffy <josephduffy@me.com>
2018-10-15 23:20:12 +0100

    Rename `CustomisableUpdateIntervalSource` to `CustomisableUpdateIntervalControllable`
    This should add `Controllable` to the remaining `Source`s

--- Source/Sources/Device Motion/Gyroscope.swift
+++ Source/Sources/Device Motion/Gyroscope.swift
@@ -1,7 +1,7 @@
 import Foundation
 import CoreMotion
 
-public final class Gyroscope: BaseSource, CustomisableUpdateIntervalSource, ValuesProvider {
+public final class Gyroscope: BaseSource, Source, CustomisableUpdateIntervalControllable, ValuesProvider {
 
     public static var defaultUpdateInterval: TimeInterval = 1
 
