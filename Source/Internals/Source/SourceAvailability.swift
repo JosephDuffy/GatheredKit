839e0f6e0bff8a631ae9f5882dd63c4cc1079d6c
Joseph Duffy <commits@josephduffy.co.uk>
2019-04-07 21:13:44 +0100

    Update `SourceAvailability` documentation

--- Source/Internals/Source/SourceAvailability.swift
+++ Source/Internals/Source/SourceAvailability.swift
@@ -5,11 +5,15 @@ public enum SourceAvailability {
 
     /// The source requires the user's permission to run, but has
     /// not yet asked the user for permission.
+    ///
     /// The user will be prompted for permissions when the `startUpdating`
     /// function is called.
     case requiresPermissionsPrompt
 
-    /// The user has denied permission to access the source
+    /// This app is not authorized to use this source
+    ///
+    /// The user cannot change this app’s status, possibly due to active
+    /// restrictions such as parental controls being in place.
     case restricted
 
     /// The user has denied permission to access the source
