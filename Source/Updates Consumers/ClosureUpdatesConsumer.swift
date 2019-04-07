ab1d86c33f69c0ff38937cbd1a6852c405d237e3
Joseph Duffy <commits@josephduffy.co.uk>
2019-04-07 23:07:46 +0100

    Update `ClosureUpdatesConsumer` to call on `queue`

--- Source/Updates Consumers/ClosureUpdatesConsumer.swift
+++ Source/Updates Consumers/ClosureUpdatesConsumer.swift
@@ -2,14 +2,18 @@ public final class ClosureUpdatesConsumer: UpdatesConsumer {
     
     public typealias UpdatesClosure = ([AnyValue], AnyObject) -> Void
     
+    private let queue: OperationQueue
     private let closure: UpdatesClosure
     
     public init(queue: OperationQueue = .main, closure: @escaping UpdatesClosure) {
+        self.queue = queue
         self.closure = closure
     }
     
     public func comsume(values: [AnyValue], sender: AnyObject) {
-        closure(values, sender)
+        queue.addOperation { [closure] in
+            closure(values, sender)
+        }
     }
     
 }
