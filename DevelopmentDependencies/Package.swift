c280c0e8bf085077a25b08b1e0f1c00df86df9d4
Joseph Duffy <commits@josephduffy.co.uk>
2019-12-12 19:17:01 +0000

    Move DevelopmentDependencies to subdirectory

---
+++ DevelopmentDependencies/Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "GatheredKitDevelopmentDependencies",
+    platforms: [
+        .macOS(.v10_10),
+    ],
+    products: [
+        .library(name: "DangerDeps", type: .dynamic, targets: ["DangerDependencies"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/danger/swift.git", from: "2.0.0"),
+        .package(url: "https://github.com/JosephDuffy/SwiftChecksDangerPlugin.git", from: "0.1.0"),
+        .package(url: "https://github.com/Realm/SwiftLint", .upToNextMinor(from: "0.36.0")),
+    ],
+    targets: [
+        .target(name: "DangerDependencies", dependencies: ["Danger", "swiftlint", "danger-swift", "SwiftChecksDangerPlugin"], path: "DangerDependencies"), // dev
+    ]
+)
--- Package.swift
+++ Package.swift
@@ -12,14 +12,10 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/Quick/Quick.git", from: "2.0.0"), // dev
         .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.0"), // dev
-        .package(url: "https://github.com/danger/swift.git", from: "1.0.0"), // dev
-        .package(url: "https://github.com/Realm/SwiftLint", from: "0.32.0"), // dev
-        .package(url: "https://github.com/f-meloni/Rocket", from: "0.1.0"), // dev
     ],
     targets: [
         .target(name: "GatheredKit"),
         .testTarget(name: "GatheredKitTests", dependencies: ["GatheredKit", "Quick", "Nimble"]), // dev
-        .target(name: "CIDependencies", dependencies: ["Danger", "swiftlint", "danger-swift"], path: "Resources"), // dev
     ],
     swiftLanguageVersions: [.v5]
 )
