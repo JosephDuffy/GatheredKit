268eaa19194a8d0f1c7b730f779b232a35c9d5ea
Joseph Duffy <commits@josephduffy.co.uk>
2021-06-10 14:08:33 +0100

    Add convenience functions for adding listeners

--- Sources/GatheredKit/Source/UpdatingSource.swift
+++ Sources/GatheredKit/Source/UpdatingSource.swift
@@ -1,7 +1,22 @@
+#if canImport(Combine)
+import Combine
+#endif
+
 public protocol UpdatingSource: Source {
     /// An update publisher that can be used to subscribe to events emitted by he controllable.
     var sourceEventPublisher: AnyUpdatePublisher<SourceEvent> { get }
 
     /// A boolean indicating if the source is currently performing automatic updates.
     var isUpdating: Bool { get }
+}
+
+extension UpdatingSource {
+    @available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+    public var publisher: AnyPublisher<SourceEvent, Never> {
+        sourceEventPublisher.combinePublisher
+    }
+
+    public func addUpdateListener(_ updateListener: @escaping AnyUpdatePublisher<SourceEvent>.UpdateListener) -> Subscription {
+        sourceEventPublisher.addUpdateListener(updateListener)
+    }
 }
