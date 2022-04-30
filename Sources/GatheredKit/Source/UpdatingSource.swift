24bdbf8932ce0fb0d4663c58c70d696f813f1815
Joseph Duffy <commits@josephduffy.co.uk>
2022-04-29 19:11:06 -0700

    Add `AsyncStream` of `SourceEvent` to `UpdatingSource`

--- Sources/GatheredKit/Source/UpdatingSource.swift
+++ Sources/GatheredKit/Source/UpdatingSource.swift
@@ -7,11 +7,25 @@ public protocol UpdatingSource: Source {
     /// the source.
     var sourceEventPublisher: AnyUpdatePublisher<SourceEvent> { get }
 
+    /// An `AsyncStream` of events emitted by this ``UpdatingSource``.
+    var sourceEvents: AsyncStream<SourceEvent> { get }
+
     /// A boolean indicating if the source is currently performing automatic updates.
     var isUpdating: Bool { get }
 }
 
 extension UpdatingSource {
+    public var sourceEvents: AsyncStream<SourceEvent> {
+        AsyncStream<SourceEvent> { continuation in
+            let cancellable = self.sourceEventPublisher.addUpdateListener { event in
+                continuation.yield(event)
+            }
+            continuation.onTermination = { @Sendable [cancellable] _ in
+                cancellable.cancel()
+            }
+        }
+    }
+
     public var publisher: AnyPublisher<SourceEvent, Never> {
         sourceEventPublisher.combinePublisher
     }
