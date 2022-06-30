30d4a458575d9f240254c4f688ba31b2d6b7bb10
Joseph Duffy <commits@josephduffy.co.uk>
2022-06-30 15:21:23 -0700

    Remove `Sendable` inheritance

--- Sources/GatheredKit/PropertiesProvider/PropertiesProviding.swift
+++ Sources/GatheredKit/PropertiesProvider/PropertiesProviding.swift
@@ -1,5 +1,5 @@
 /// An object that provides properties.
-public protocol PropertiesProviding: AnyObject, Sendable {
+public protocol PropertiesProviding: AnyObject {
     /// An array of all the properties provided by this object
     var allProperties: [AnyProperty] { get }
 }
