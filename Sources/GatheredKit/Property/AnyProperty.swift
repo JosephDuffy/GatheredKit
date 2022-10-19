4f669aea586f11f4e818575bfcfd58dba4b38301
Joseph Duffy <commits@josephduffy.co.uk>
2022-10-19 21:18:48 +0100

    Remove duplicate requirement

--- Sources/GatheredKit/Property/AnyProperty.swift
+++ Sources/GatheredKit/Property/AnyProperty.swift
@@ -5,7 +5,6 @@ public protocol AnyProperty: AnyObject, AnySnapshot {
     typealias AnyUpdateListener = (_ snapshot: AnySnapshot) -> Void
 
     var displayName: String { get }
-    var date: Date { get }
     var error: Error? { get }
     var errorsPublisher: AnyPublisher<Error?, Never> { get }
     var typeErasedFormatter: Formatter { get }
