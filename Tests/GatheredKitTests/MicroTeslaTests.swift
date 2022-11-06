20e2030a0f963fb4c8a849eaca3ddf83b31dc06b
Joseph Duffy <commits@josephduffy.co.uk>
2022-11-05 18:29:16 -0700

    Add failing test

--- Tests/GatheredKitTests/MicroTeslaTests.swift
+++ Tests/GatheredKitTests/MicroTeslaTests.swift
@@ -5,5 +5,6 @@ final class MicroTeslaTests: XCTestCase {
     func testMicroTesla() {
         let microTesla = UnitMagneticField.microTesla
         XCTAssertEqual(microTesla.converter.baseUnitValue(fromValue: 1000), 1)
+        XCTAssertTrue(false)
     }
 }
