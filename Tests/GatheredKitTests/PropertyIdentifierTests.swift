8f8393a8346c253bf1d2a5c8722fa005c27571a1
Joseph Duffy <commits@josephduffy.co.uk>
2022-11-05 18:09:15 -0700

    Add more tests

--- Tests/GatheredKitTests/PropertyIdentifierTests.swift
+++ Tests/GatheredKitTests/PropertyIdentifierTests.swift
@@ -13,4 +13,38 @@ final class PropertyIdentifierTests: XCTestCase {
         XCTAssertEqual(sourceIdentifier.deviceIdentifier, "device-id")
         XCTAssertEqual(propertyIdentifier.description, identifierString)
     }
+
+    func testLosslessStringConvertibleConformanceWithoutDeviceId() throws {
+        let identifierString = "TestsNamespace.TestSource|test-instance/property-1/property-2/final-property"
+        let propertyIdentifier = try XCTUnwrap(PropertyIdentifier(identifierString))
+        let sourceIdentifier = propertyIdentifier.sourceIdentifier()
+        XCTAssertEqual(sourceIdentifier.namespace, "TestsNamespace")
+        XCTAssertEqual(sourceIdentifier.sourceKind.rawValue, "TestSource")
+        XCTAssertEqual(sourceIdentifier.instanceIdentifier?.id, "test-instance")
+        XCTAssertFalse(sourceIdentifier.instanceIdentifier?.isTransient ?? true)
+        XCTAssertNil(sourceIdentifier.deviceIdentifier)
+        XCTAssertEqual(propertyIdentifier.description, identifierString)
+    }
+
+    func testLosslessStringConvertibleConformanceWithoutInstanceIdentifier() throws {
+        let identifierString = "TestsNamespace.TestSource@device-id/property-1/property-2/final-property"
+        let propertyIdentifier = try XCTUnwrap(PropertyIdentifier(identifierString))
+        let sourceIdentifier = propertyIdentifier.sourceIdentifier()
+        XCTAssertEqual(sourceIdentifier.namespace, "TestsNamespace")
+        XCTAssertEqual(sourceIdentifier.sourceKind.rawValue, "TestSource")
+        XCTAssertNil(sourceIdentifier.instanceIdentifier)
+        XCTAssertEqual(sourceIdentifier.deviceIdentifier, "device-id")
+        XCTAssertEqual(propertyIdentifier.description, identifierString)
+    }
+
+    func testLosslessStringConvertibleConformanceWithoutInstanceIdentifierOrDeviceId() throws {
+        let identifierString = "TestsNamespace.TestSource/property-1/property-2/final-property"
+        let propertyIdentifier = try XCTUnwrap(PropertyIdentifier(identifierString))
+        let sourceIdentifier = propertyIdentifier.sourceIdentifier()
+        XCTAssertEqual(sourceIdentifier.namespace, "TestsNamespace")
+        XCTAssertEqual(sourceIdentifier.sourceKind.rawValue, "TestSource")
+        XCTAssertNil(sourceIdentifier.instanceIdentifier)
+        XCTAssertNil(sourceIdentifier.deviceIdentifier)
+        XCTAssertEqual(propertyIdentifier.description, identifierString)
+    }
 }
--- Tests/GatheredKitTests/SourceIdentifierTests.swift
+++ Tests/GatheredKitTests/SourceIdentifierTests.swift
@@ -12,4 +12,35 @@ final class SourceIdentifierTests: XCTestCase {
         XCTAssertEqual(sourceIdentifier.deviceIdentifier, "device-id")
         XCTAssertEqual(sourceIdentifier.description, identifierString)
     }
+
+    func testLosslessStringConvertibleConformanceWithoutDeviceId() throws {
+        let identifierString = "TestsNamespace.TestSource|test-instance"
+        let sourceIdentifier = try XCTUnwrap(SourceIdentifier(identifierString))
+        XCTAssertEqual(sourceIdentifier.namespace, "TestsNamespace")
+        XCTAssertEqual(sourceIdentifier.sourceKind.rawValue, "TestSource")
+        XCTAssertEqual(sourceIdentifier.instanceIdentifier?.id, "test-instance")
+        XCTAssertFalse(sourceIdentifier.instanceIdentifier?.isTransient ?? true)
+        XCTAssertNil(sourceIdentifier.deviceIdentifier)
+        XCTAssertEqual(sourceIdentifier.description, identifierString)
+    }
+
+    func testLosslessStringConvertibleConformanceWithoutInstanceIdentifier() throws {
+        let identifierString = "TestsNamespace.TestSource@device-id"
+        let sourceIdentifier = try XCTUnwrap(SourceIdentifier(identifierString))
+        XCTAssertEqual(sourceIdentifier.namespace, "TestsNamespace")
+        XCTAssertEqual(sourceIdentifier.sourceKind.rawValue, "TestSource")
+        XCTAssertNil(sourceIdentifier.instanceIdentifier)
+        XCTAssertEqual(sourceIdentifier.deviceIdentifier, "device-id")
+        XCTAssertEqual(sourceIdentifier.description, identifierString)
+    }
+
+    func testLosslessStringConvertibleConformanceWithoutInstanceIdentifierOrDeviceId() throws {
+        let identifierString = "TestsNamespace.TestSource"
+        let sourceIdentifier = try XCTUnwrap(SourceIdentifier(identifierString))
+        XCTAssertEqual(sourceIdentifier.namespace, "TestsNamespace")
+        XCTAssertEqual(sourceIdentifier.sourceKind.rawValue, "TestSource")
+        XCTAssertNil(sourceIdentifier.instanceIdentifier)
+        XCTAssertNil(sourceIdentifier.deviceIdentifier)
+        XCTAssertEqual(sourceIdentifier.description, identifierString)
+    }
 }
